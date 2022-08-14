ec699bbd6d7533a74de6830d1403029e670f1282
joynal67598 <joynalam67598@gmail.com>
2022-08-14 22:22:05 +0600

    add defatult web host builder

--- TouristPlaceWebApi/Program.cs
+++ TouristPlaceWebApi/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace TouristPlaceWebApi // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webhost =>
+            {
+                webhost.UseStartup<Startup>();
+            });
+
+        }
+    }
+}
